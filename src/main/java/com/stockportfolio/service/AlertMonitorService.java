c5fa5d979258de3faa036fb1b41ae423a39abfd5
VishalUmasankar <163153848+VishalUmasankar@users.noreply.github.com>
2025-05-30 17:35:12 +0530

    Update AlertMonitorService.java

--- src/main/java/com/stockportfolio/service/AlertMonitorService.java
+++ src/main/java/com/stockportfolio/service/AlertMonitorService.java
@@ -35,11 +35,11 @@ public class AlertMonitorService {
 
             if ((above != null && currentPrice > above) || (below != null && currentPrice < below)) {
                 User user = userRepository.findById(h.getId()).orElse(null);
-                if (user != null && user.getemail() != null) {
+                if (user != null && user.getEmail() != null) {
                     String subject = "Stock Alert: " + h.getStockSymbol();
                     String body = String.format("Current price of %s is %.2f which crossed your set limit!",
                             h.getStockSymbol(), currentPrice);
-                    emailService.sendAlertMail(user.getemail(), subject, body);
+                    emailService.sendAlertMail(user.getEmail(), subject, body);
                 }
             }
         }
